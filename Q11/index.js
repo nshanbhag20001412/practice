57680a3577078b5d17bfa5a3a03c2f8983e45629
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-08-17 13:29:42 +0530

    Completed Q11
    Create a password checker web app. If password is lesser than 10 characters then show an error to user otherwise show success. Someone can ask to make the submit button disabled. Some can ask to make the input field green or red depending on input.

---
+++ Q11/index.js
@@ -0,0 +1,46 @@
+const inputText=document.querySelector(".input-text");
+const checkBtn=document.querySelector(".check");
+const display=document.querySelector(".display");
+
+if(checkBtn){
+
+    checkBtn.addEventListener("click",checkPassword);
+}
+
+function checkPassword(){
+
+    var password=inputText.value;
+    
+    if(password.length<10){
+        
+        display.innerHTML="Minimum length of password should be 10!";
+        inputText.style.backgroundColor="red";
+    }
+    else{
+        
+        var cnt=0;
+        for(i=0;i<password.length;i++){
+
+            if(password[i]!=='@'){
+               continue;
+            }
+            else{
+                cnt++;
+                break;
+            }
+        }
+
+        if(cnt>0){
+
+           display.innerHTML="Password is good!";
+           inputText.style.backgroundColor="green";
+        }
+        else{
+
+            inputText.style.backgroundColor="red";
+            display.innerHTML="Password should contain @";
+        }
+
+
+    }
+}
