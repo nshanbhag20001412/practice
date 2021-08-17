f2597de6589df23645417c4a3f4bfed1269ae818
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-08-17 11:23:26 +0530

    Q7 in process

---
+++ Q7/index.js
@@ -0,0 +1,28 @@
+const inputText=document.querySelector(".text");
+const callBtn=document.querySelector(".call");
+const display=document.querySelector(".display");
+
+var serverUrl="#";
+
+function clickEventHandler(){
+
+    var inputContent = inputText.value;
+
+    fetch(callAPI(inputContent))
+    .then(response => response.json())
+    .then(json => {
+        var receivedText = json.contents.translated;
+        display.innerText = receivedText; // output
+       })
+}
+
+ function callAPI(inputContent){
+    console.log(inputContent);
+    return serverUrl + inputContent;
+ }
+
+
+if(callBtn){
+ 
+ callBtn.addEventListener("click", clickEventHandler);
+}
