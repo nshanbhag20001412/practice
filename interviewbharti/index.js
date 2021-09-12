bdb3d4ff58bcdfd9ca371a197c53b9d6ebdae848
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-09-12 21:53:57 +0530

    updates

--- interviewbharti/index.js
+++ interviewbharti/index.js
@@ -8,5 +8,8 @@ fetch("https://jsonplaceholder.typicode.com/todos")
         var titleText=array.title;
          if(array.completed===false)
         content.innerHTML+=`\n <p style="color:grey">Tittle:${titleText}</p>  \n Flag:${array.completed}`;
+        else{
+        content.innerHTML+=`\nTittle:${titleText} \n Flag:${array.completed}`;   
+        }
     })
 })
