c1d77cb86e8ee0867abfdd72576e83b98580c23f
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-09-22 21:26:43 +0530

    updates to index.js

--- sampleinterviewquestions/QuestionsftAbhishek/Q7/index.js
+++ sampleinterviewquestions/QuestionsftAbhishek/Q7/index.js
@@ -2,8 +2,8 @@ var passwordInput=document.querySelector("#password-input");
 var btnToggle=document.querySelector("#toggle");
 
 function changeInputBorder(){
-
  if(passwordInput.value.length>10)
+   
    passwordInput.style.borderColor="green"
  else
    passwordInput.style.borderColor="black"
