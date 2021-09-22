9c8f75027e33bd01d6cf8baf66343ea9d628587b
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-09-22 20:54:02 +0530

    Revert "Q8"
    This reverts commit 34f13504348cada03e31f79299fd9190d4cdc5e5.

--- sampleinterviewquestions/QuestionsftAbhishek/Q8/index.js
+++
@@ -1,6 +0,0 @@
-var randomNameOutput=document.querySelector(".random-name");
-
-var nameList=['Neha','Abhay','Rekha','Gajanan','Shruti','Elian','Nilesh','Sidhant','Shreyashi','Rutwij']
-
-var randomName=nameList.Math.random();
-console.log(randomName);
